3bc7387122ade4a0ed833b6401d265386e98974e
FingerPrintGames <kevinjordan199449@gmail.com>
2022-09-21 10:52:07 -0400

    Make journals__card larger for mobile

--- view/renderEntries.js
+++ view/renderEntries.js
@@ -4,10 +4,10 @@ const noEntriesHTML = `<div class="journals__main">
 <div class="journals__card">
 <button class="button entries__add-button">Add Entry</button>
   <p>No Entries yet...</p>
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 entries__back-arrow">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-</svg>
-</div>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 entries__back-arrow">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+  </svg>
 </div>`;
 
 function generateEntriessHTML(entries = []) {
@@ -23,10 +23,10 @@ function generateEntriessHTML(entries = []) {
         </div>`;
         })
         .join(" ")}
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6             entries__back-arrow">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-            </svg>
         </div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6             entries__back-arrow">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                </svg>
     </div>`;
 }
 
